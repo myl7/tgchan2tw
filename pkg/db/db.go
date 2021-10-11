6bd6baa98f94c17e03d15f19249aa75edf8690ff
myl7 <myl@myl.moe>
2021-10-11 15:54:53 +0800

    refactor: Use goembed to include sql schema

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"database/sql"
+	_ "embed"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/myl7/tgchan2tw/pkg/conf"
 	"log"
@@ -11,18 +12,8 @@ func GetDB() (*sql.DB, error) {
 	return sql.Open("sqlite3", conf.DBPath)
 }
 
-const schema = `
-CREATE TABLE IF NOT EXISTS items (
-  id INTEGER(4) PRIMARY KEY
-);
-CREATE TABLE IF NOT EXISTS msgs (
-  id INTEGER(8) PRIMARY KEY
-);
-CREATE TABLE IF NOT EXISTS item2msg (
-  item_id INTEGER(4) REFERENCES items (id),
-  msg_id INTEGER(8) REFERENCES msgs (id)
-);
-`
+//go:embed schema.sql
+var schema string
 
 func init() {
 	db, err := GetDB()
