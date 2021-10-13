bb5e07a5675cb5780558ee0862bec570bfa6ea74
myl7 <myl@myl.moe>
2021-10-13 22:01:48 +0800

    build: Fix RUN in scripts which from dockerfile

--- scripts/setup-dep.sh
+++ scripts/setup-dep.sh
@@ -6,6 +6,6 @@ set -euo pipefail
 wget -qO twitter-text-parse-go-1.0.0.tar.gz https://github.com/myl7/twitter-text-parse-go/archive/v1.0.0.tar.gz
 tar xzf twitter-text-parse-go-1.0.0.tar.gz
 mkdir twitter-text-parse-go-1.0.0/lib
-RUN wget -qO twitter-text-parse-go-1.0.0/lib/libtwitter_text_parse_go.a https://github.com/myl7/twitter-text-parse-go/releases/download/v1.0.0/libtwitter_text_parse_go-x86_64-unknown-linux-$1.a
+wget -qO twitter-text-parse-go-1.0.0/lib/libtwitter_text_parse_go.a https://github.com/myl7/twitter-text-parse-go/releases/download/v1.0.0/libtwitter_text_parse_go-x86_64-unknown-linux-$1.a
 mkdir -p third_party
-RUN mv twitter-text-parse-go-1.0.0 third_party/twitter-text-parse-go
+mv twitter-text-parse-go-1.0.0 third_party/twitter-text-parse-go
