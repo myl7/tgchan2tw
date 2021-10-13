214378b8a0bd9d0daf792c102cd1c248ebf2c54c
myl7 <myl@myl.moe>
2021-10-13 22:00:45 +0800

    build: Use explicit name for wget result

--- scripts/setup-dep.sh
+++ scripts/setup-dep.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # $1 should be gnu/musl
 
-wget -q https://github.com/myl7/twitter-text-parse-go/archive/v1.0.0.tar.gz
+wget -qO twitter-text-parse-go-1.0.0.tar.gz https://github.com/myl7/twitter-text-parse-go/archive/v1.0.0.tar.gz
 tar xzf twitter-text-parse-go-1.0.0.tar.gz
 mkdir twitter-text-parse-go-1.0.0/lib
 RUN wget -qO twitter-text-parse-go-1.0.0/lib/libtwitter_text_parse_go.a https://github.com/myl7/twitter-text-parse-go/releases/download/v1.0.0/libtwitter_text_parse_go-x86_64-unknown-linux-$1.a
