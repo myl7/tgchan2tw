5df7a2adfe5d10ef56f143029e4727f045f5846d
myl7 <myl@myl.moe>
2021-10-22 05:06:35 +0800

    feat: Add blockquote support

--- pkg/fetch/filter.go
+++ pkg/fetch/filter.go
@@ -21,8 +21,41 @@ func FilterText(body string) (ItemBody, error) {
 		return ItemBody{}, err
 	}
 
-	var imageUrls []string
 	d := goquery.NewDocumentFromNode(h)
+	imageUrls := filterImageUrls(d)
+	quoteUrl, isForward := filterHeadQuote(d)
+
+	var blocks []string
+	d.Find("body").Children().Each(func(_ int, s *goquery.Selection) {
+		t := ""
+		if goquery.NodeName(s) == "p" {
+			t = filterBodyP(s)
+		} else if goquery.NodeName(s) == "pre" {
+			t = filterBodyPre(s)
+		}
+		if t != "" {
+			blocks = append(blocks, t)
+		}
+	})
+
+	res := ""
+	if isForward {
+		res = quoteUrl
+		quoteUrl = ""
+	} else {
+		res = strings.Join(blocks, "\n")
+	}
+
+	return ItemBody{
+		Text:      res,
+		ImageUrls: imageUrls,
+		ReplyUrl:  quoteUrl,
+		IsForward: isForward,
+	}, nil
+}
+
+func filterImageUrls(d *goquery.Document) []string {
+	var imageUrls []string
 	d.Find("img").Each(func(_ int, s *goquery.Selection) {
 		ok := true
 		s.Parents().Each(func(_ int, s *goquery.Selection) {
@@ -39,61 +72,68 @@ func FilterText(body string) (ItemBody, error) {
 			return
 		}
 
+		s.Remove()
 		imageUrls = append(imageUrls, src)
 	})
+	return imageUrls
+}
+
+func filterHeadQuote(d *goquery.Document) (string, bool) {
+	r := d.Find("body").Children().First()
+
+	quoteUrl := ""
+	href, ok := r.Find("a").Attr("href")
+	if ok {
+		quoteUrl = href
+	}
+
+	if goquery.NodeName(r) == "blockquote" {
+		// Ensure it is reply message
+		r.Remove()
+		return quoteUrl, false
+	}
 
 	isForward := false
-	forwardLink := ""
-	var blocks []string
-	d.Find("body > p").Each(func(_ int, s *goquery.Selection) {
-		t := ""
-		s.Contents().Each(func(i int, p *goquery.Selection) {
-			if goquery.NodeName(p) == "br" {
-				t += "\n"
-			} else if t == "" {
-				t += p.Text()
-			} else {
-				t += " " + p.Text()
-			}
-		})
-		ts := strings.Split(t, "\n")
-		for i := range ts {
-			ts[i] = strings.TrimSpace(ts[i])
-		}
-		t = strings.Join(ts, "\n")
-		t = strings.TrimSpace(t)
+	if strings.HasPrefix(r.Text(), "Forwarded From") {
+		// Ensure it is forward message
+		isForward = true
+	}
 
-		if strings.HasPrefix(t, "Forwarded From") {
-			href, ok := s.Find("a").Attr("href")
-			if ok {
-				isForward = true
-				forwardLink = href
-			}
-		}
+	if !isForward {
+		// It is a plain message, so reset quoteUrl
+		quoteUrl = ""
+	}
 
-		blocks = append(blocks, t)
+	return quoteUrl, isForward
+}
+
+func filterBodyP(s *goquery.Selection) string {
+	t := ""
+	s.Contents().Each(func(i int, p *goquery.Selection) {
+		if goquery.NodeName(p) == "br" {
+			t += "\n"
+		} else {
+			t += p.Text()
+		}
 	})
+	ts := strings.Split(t, "\n")
+	for i := range ts {
+		ts[i] = strings.TrimSpace(ts[i])
+	}
+	t = strings.Join(ts, "\n")
+	t = strings.TrimSpace(t)
+	return t
+}
 
-	replyUrl := ""
-	r := d.Find("body :first-child")
-	if goquery.NodeName(r) == "blockquote" {
-		href, ok := r.Find("a").Attr("href")
-		if ok {
-			replyUrl = href
+func filterBodyPre(s *goquery.Selection) string {
+	t := ""
+	s.Contents().Each(func(_ int, s *goquery.Selection) {
+		if goquery.NodeName(s) == "br" {
+			t += "\n"
+		} else {
+			t += s.Text()
 		}
-	}
-
-	var res string
-	if isForward {
-		res = forwardLink
-	} else {
-		res = strings.Join(blocks, "\n")
-	}
-
-	return ItemBody{
-		Text:      res,
-		ImageUrls: imageUrls,
-		ReplyUrl:  replyUrl,
-		IsForward: isForward,
-	}, nil
+	})
+	t = strings.TrimRight(t, "\n")
+	return t
 }
